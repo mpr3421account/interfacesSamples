8c0a18d1c0762fcbf292739f7417fb7be35fd0c0
Marcos Ribeiro <marcos@azacessorios.com.br>
2023-01-19 08:54:20 -0300

    Solução sem Interface.

--- interfacesSamples/Program.cs
+++ interfacesSamples/Program.cs
@@ -9,3 +9,26 @@ deve então gerar a nota de pagamento (contendo valor da locação, valor do
 imposto e valor total do pagamento) e informar os dados na tela. Veja os
 exemplos.
 */
+using interfacesSamples.Entities;
+using interfacesSamples.Services;
+using System.Globalization;
+
+Console.WriteLine("Enter rental data");
+Console.Write("Car model: ");
+string model = Console.ReadLine();
+Console.Write("Pickup (dd/MM/yyyy hh:mm) ");
+DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy hh:mm", CultureInfo.InvariantCulture);
+Console.Write("Return (dd/MM/yyyy hh:mm) ");
+DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy hh:mm", CultureInfo.InvariantCulture);
+
+Console.Write("Enter price per hour: ");
+double hour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.Write("Enter price per day: ");
+double day = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+
+RentalService rentalService = new RentalService(hour, day);
+rentalService.ProcessInvoice(carRental);
+Console.WriteLine("INVOCE:");
+Console.WriteLine(carRental.Invoice);
